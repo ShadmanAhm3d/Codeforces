711987e0f5f08c6ddf242df471f7f7c535b410f9
ShadmanAhm3d <shadman01010@gmail.com>
2024-06-01 18:04:34 +0530

    Push June

---
+++ june/turle.cpp
@@ -0,0 +1,23 @@
+#pragma GCC optimize("O3")
+#include <bits/stdc++.h>
+#include<ext/pb_ds/assoc_container.hpp>
+#include<ext/pb_ds/tree_policy.hpp>
+#define mod 1000000007
+#define endl '\n'
+#define ll  long long
+#define pb push_back
+using namespace std;
+
+
+
+
+int main(){
+
+  int tc ; cin >> tc;
+  while(tc--){
+     int l ,r; cin>>l >> r;
+     cout << __lg(r) << endl;
+  }
+
+ return 0;
+}
